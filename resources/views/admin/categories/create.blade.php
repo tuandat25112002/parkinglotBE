b87fc8dcfdf3494bedd3e453d16a534e8b87f97c
Nguyen Van Dung <86200342+dungkpht123@users.noreply.github.com>
2023-12-05 19:03:23 +0700

    Update create.blade.php

--- resources/views/admin/categories/create.blade.php
+++ resources/views/admin/categories/create.blade.php
@@ -1,26 +1,16 @@
 @extends('admin.layouts.master')
 @section('content')
 <div class="main-panel">
-          <div class="content-wrapper">
-            <div class="page-header">
-              <h3 class="page-title">All Categories </h3>
-              <!-- <nav aria-label="breadcrumb">
-                <ol class="breadcrumb">
-                    <li class="breadcrumb-item"><a href="{{ route('categories.create') }}">Add Category</a></li>
-                </ol>
-            </nav> -->
-            </div>
-            <div class="row">
-
-              <div class="col-lg-12 grid-margin stretch-card">
+    <div class="content-wrapper">
+        <div class="page-header">
+            <h3 class="page-title"> Add Categories </h3>
+        </div>
+        <div class="row">
+            <div class="col-12 grid-margin stretch-card">
                 <div class="card">
-                  <div class="card-body">
-                  <div class="col-md-12 text-right mb-3" style=" margin-left: 40px;">
-                    <a href="{{ route('categories.create') }}" class="btn btn-primary">Add Category</a>
-                </div>
-                <div class="card-body">
-                    <div class="col-md-12 text-right mb-3">
-                    @if ($errors->any())
+                    <div class="card-body">
+                        <h4 class="card-title">Add Categories</h4>
+                        @if ($errors->any())
                             <div class="alert alert-danger">
                                 <ul>
                                     @foreach ($errors->all() as $error)
@@ -34,131 +24,45 @@
                                 {{session('status')}} <i id="close" class="fas fa-times float-right mt-1"></i>
                             </div>
                         @endif
+                        <form class="forms-sample" method="POST" action="{{ route('categories.store') }}">
+                            @csrf
+                            <div class="form-group">
+                                <label for="exampleInputName1">Name : </label>
+                                <input type="text" class="form-control" id="exampleInputName1" name="name" placeholder="Name">
+                            </div>
+                            <div class="form-group">
+                                <label for="">Category Status</label>
+                                <div class="form-check">
+                                    <label class="radio-label">
+                                        <input type="radio" name="status" id="public" value="0" checked />
+                                        <span class="radio-custom"></span>
+                                        <span class="radio-text">Public</span>
+                                    </label>
+                                    <label class="radio-label">
+                                        <input type="radio" name="status" id="private" value="1" checked />
+                                        <span class="radio-custom"></span>
+                                        <span class="radio-text">Private</span>
+                                    </label>
+                                </div>
+                            </div>
+                            <button type="submit" class="btn btn-gradient-primary mr-2">Add</button>
+                            <a href="{{ route('categories.index') }}" class="btn btn-light">Cancel</a>
+                        </form>
+                    </div>
                 </div>
-                    <table class="table table-bordered">
-                      <thead>
-                        <tr>
-                          <th> # </th>
-                          <th> Name </th>
-                          <th> Status </th>
-                        <th>Action</th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                      @foreach ($categories as $category)
-					<tr>
-						<th>{{ $category->id }}</th>
-						<td>{{ $category->name }}</td>
-                        <td>
-                            @if($category->status == 1)
-                                <span class="badge badge-danger">Private</span>
-                            @else
-                                <span class="badge badge-success">Public</span>
-                            @endif
-                        </td>
-
-
-
-                        <td>
-            <form method="POST" action="{{ route('categories.destroy', $category->id) }}">
-                @csrf
-                @method('DELETE')
-                <a href="{{ route('categories.edit', $category->id) }}" class="btn btn-success">
-                    Sửa
-                </a>
-                <button type="submit" class="btn btn-danger" onclick="return confirm('Are you sure you want to delete this category?')">Xoá</button>
-            </form>
-        </td>
-					</tr>
-					@endforeach
-                      </tbody>
-                    </table>
-                  </div>
-                </div>
-              </div>
-              <div class="col-lg-12 grid-margin stretch-card">
-                <div class="card">
-
-                </div>
-              </div>
-
             </div>
-          </div>
-          <!-- content-wrapper ends -->
-          <!-- partial:../../partials/_footer.html -->
-          <footer class="footer">
-            <div class="container-fluid clearfix">
-              <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright © bootstrapdash.com 2020</span>
-              <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center"> Free <a href="https://www.bootstrapdash.com/bootstrap-admin-template/" target="_blank">Bootstrap admin template</a> from Bootstrapdash.com</span>
-            </div>
-          </footer>
-          <!-- partial -->
         </div>
-        <script type="text/javascript">
-$('.checkbox-active').click(function(){
-  var id= $(this).data('id');
-  var _token = $('input[name="_token"]').val();
-  $.ajax({
-    url:"{{URL::to('category/update_active')}}",
-    method:"POST",
-    data: {id:id,_token:_token},
-    success:function(data){
-      alert(data.message);
-    }
-  });
-});
-</script>
-<style>
-    /* Style for radio buttons */
-.form-check {
-    margin-top: 8px;
-}
+    </div>
+    <!-- content-wrapper ends -->
+    <!-- partial:../../partials/_footer.html -->
+    <footer class="footer">
+        <div class="container-fluid clearfix">
+            <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright © bootstrapdash.com 2020</span>
+            <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center"> Free <a href="https://www.bootstrapdash.com/bootstrap-admin-template/" target="_blank">Bootstrap admin template</a> from Bootstrapdash.com</span>
+        </div>
+    </footer>
+    <!-- partial -->
+</div>
 
-.radio-label {
-    display: inline-block;
-    vertical-align: middle;
-    position: relative;
-    padding-left: 25px;
-    margin-right: 15px;
-    cursor: pointer;
-    font-size: 16px;
-}
-
-.radio-label input {
-    position: absolute;
-    opacity: 0;
-    cursor: pointer;
-}
-
-.radio-custom {
-    position: absolute;
-    top: 0;
-    left: 0;
-    height: 18px;
-    width: 18px;
-    background-color: #fff;
-    border: 1px solid #999;
-    border-radius: 50%;
-}
-
-.radio-text {
-    margin-left: 30px;
-}
-
-/* Style for checked radio buttons */
-.radio-label input:checked ~ .radio-custom:after {
-    content: '';
-    position: absolute;
-    display: block;
-    top: 4px;
-    left: 4px;
-    width: 10px;
-    height: 10px;
-    border-radius: 50%;
-    background: #333;
-}
-
-</style>
-          <!-- content-wrapper ends -->
+<!-- content-wrapper ends -->
 @endsection
-?>
